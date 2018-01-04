0c9cf75af31600b14a02255c497bde0a3f2543bd
Wesley <contato@wesleyteixeira.com.br>
2018-01-04 02:10:50 -0300

    Juntando array merge model cadastro

--- app/models/User.php
+++ app/models/User.php
@@ -41,23 +41,20 @@ class User extends \HXPHP\System\Model
 
 		//Recupera o role_id de user
 		$role = Role::find_by_role('User');
-
 		if (is_null($role)) {
 			array_push($callbackObj->errors, 'A role user não existe. Contato o administrator');
 			return $callbackObj;
 		}
-
-		//Insere os dados recuperado do role no array do post
-		$post = array_merge($post, array(
+		$user_data = array(
 			'role_id' => $role->id,
 			'status' => 1
-		));
+		);
 
 		//Cria a senha criptografada usando o HXPHP
 		$password = \HXPHP\System\Tools::hashHX($post['password']);
 
-		//Insere a senha criptografada no array do post
-		$post = array_merge($post, $password);
+		//Insere a senha criptografada e role no array do post
+		$post = array_merge($post, $user_data, $password);
 
 		//Salva os dados no banco de dados
 		$cadastrar = self::create($post);
